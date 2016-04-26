1974695b3009fa89a14f7a27396bfb499d75bd20
Evandro Eisinger <evandro.eisinger@corp.globo.com>
2016-04-26 11:06:06 -0300

    refactor: improve async decorator

--- async_responses.py
+++ async_responses.py
@@ -1,16 +1,45 @@
 # -*- coding: utf-8 -*-
 
+import yaml
+
+from io import StringIO
+
+from unittest.mock import patch
+
+from tornadoalf.client import Client
+
+from tornado.httpclient import HTTPRequest
+from tornado.httpclient import HTTPResponse
+from tornado.httpclient import AsyncHTTPClient
+from tornado.testing import gen_test
+from tornado.concurrent import Future
+from tornado import gen
+
 
 class async_responses:
 
-    @classmethod
-    def mock(cls, fixture_path):
+    @property
+    def _response(self):
+        request = HTTPRequest("")
+        status_code = self.fixture.get("code", 200)
+        headers = self.fixture.get("headers", {})
+        body = StringIO(self.fixture.get("body", ""))
 
-        def decorator(f):
+        return HTTPResponse(request, status_code, headers, body)
 
-            def wrapper(*args, **kwargs):
-                f(*args, **kwargs)
+    def __init__(self, fixture_path):
+        self.fixture = yaml.load(open(fixture_path).read())
 
-            return wrapper
+    def __call__(self, f):
+        coroutine = gen.coroutine(f)
 
-        return decorator
+        @gen_test
+        def wrapper(*args, **kargs):
+            future = Future()
+            future.set_result(self._response)
+
+            with patch.object(AsyncHTTPClient, "fetch", return_value=future):
+                with patch.object(Client, "fetch", return_value=future):
+                    yield coroutine(*args, **kargs)
+
+        return wrapper
